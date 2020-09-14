e8cc55d23d8a6ae4fb3eccca3b9a92406470ca06
Nicole Ticknor <36491978+nicoleticknor@users.noreply.github.com>
2020-09-14 13:49:31 -0700

    gitignoring node modules

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 //need to require and config this first before we try to run the app so that we get the correct variables
-const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
+const dotenv = require('dotenv');
 
-const app = require("./app");
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
