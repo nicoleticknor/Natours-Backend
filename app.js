984b450a837d3d64bebf515af0f08fb6d8401863
Nicole Ticknor <36491978+nicoleticknor@users.noreply.github.com>
2020-09-16 10:52:22 -0700

    configured and connected to mongoDB with atlas

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // ?? Express App file
 
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 
-const tourRouter = require("./routes/tourRoutes");
-const userRouter = require("./routes/userRoutes");
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
@@ -12,9 +12,9 @@ const app = express();
 //app.use adds middleware to the "middleware stack" which happens in a chain for every request (unless a specific route is specified)
 
 //to only use morgan when we are in development environment
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === 'development') {
   // NB you can use morgan's logger to save the outputs to a location
-  app.use(morgan("dev"));
+  app.use(morgan('dev'));
 }
 
 //this is express' built-in body parser
@@ -26,7 +26,7 @@ app.use(express.static(`${__dirname}/public`));
 //defining our own middleware that will apply to every request (because we don't specify a route, as we do with mounting below)
 //note that code order matters in the middleware stack - this will get executed in order, so if the route comes before this, this will never happen (because the route will send a response which terminates the request/response cycle)
 app.use((req, res, next) => {
-  console.log("hello from the middleware 🙌");
+  console.log('hello from the middleware 🙌');
   //have to call the next function at the end of our own middlware functions, otherwise the server will just hang. need to move to the next middleware function
   next();
 });
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
 
 //creating a route-specific middlware that we can mount our routes on.
 //this is how we can export these into separate files and then just call tourRouter.get('/') for that whole /api/v1/tours route piece
-app.use("/api/v1/tours", tourRouter);
-app.use("/api/v1/users", userRouter);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 module.exports = app;
