62ab5e88baf1eb873bbda10b9897d01b3dcda717
Nicole Ticknor <36491978+nicoleticknor@users.noreply.github.com>
2020-09-24 19:00:05 -0700

    implementing complete tour schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,15 +6,53 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have a name'],
     unique: true,
   },
-  rating: {
+  duration: {
     type: Number,
-    //default here just for demonstrative purposes
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
+    type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    //this is the same as excel trim, but applies only to strings
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  //this is the same as type: [String] which is an array of strings
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  //an array of start dates
+  startDates: [Date],
 });
 
 //this is the "model" that we create out of the Schema
