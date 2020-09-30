63deccbb8615d08db39c61c5324cb22995df039f
Nicole Ticknor <36491978+nicoleticknor@users.noreply.github.com>
2020-09-30 11:21:15 -0700

    added a virtual property

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,60 +1,75 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      //this is the same as excel trim, but applies only to strings
+      trim: true,
+      required: [true, 'A tour must have a summary'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+    //this is the same as type: [String] which is an array of strings
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      //this will permanently hide this field from the query results
+      select: false,
+    },
+    //an array of start dates
+    startDates: [Date],
+    //the second argument in the Schema method is the options object
   },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty'],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    //this is the same as excel trim, but applies only to strings
-    trim: true,
-    required: [true, 'A tour must have a summary'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image'],
-  },
-  //this is the same as type: [String] which is an array of strings
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    //this will permanently hide this field from the query results
-    select: false,
-  },
-  //an array of start dates
-  startDates: [Date],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+//virtual properties are ones that are calculated fields rather than ones persisted in the DB. Doing this here keeps business logic separate from application logic
+//note that we can't query anything that's not in the DB
+//use the get method because this virtual property will be created each time we use the "get" method from the database
+//getter can't have an arrow function because we need the this keyword
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
 });
 
 //this is the "model" that we create out of the Schema
