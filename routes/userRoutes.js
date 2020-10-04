42099f47397781864e579833376b8b3a9ad46524
Nicole Ticknor <36491978+nicoleticknor@users.noreply.github.com>
2020-10-04 11:25:36 -0700

    implemented user signup route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,19 @@
-const express = require("express");
+const express = require('express');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+
 const router = express.Router();
-const userController = require("../controllers/userController");
+
+//this is not RESTful, but that's okay; it doesn't make sense to have a GET to signup
+router.post('/signup', authController.signup);
 
 router
-  .route("/")
+  .route('/')
   .get(userController.getAllUsers)
   .post(userController.createUser);
 
 router
-  .route("/:id")
+  .route('/:id')
   .get(userController.getUser)
   .patch(userController.updateUser)
   .delete(userController.deleteUser);
