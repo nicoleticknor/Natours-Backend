6da1fabd869a79b6c1a7275950bf4124aefe1a56
Nicole Ticknor <36491978+nicoleticknor@users.noreply.github.com>
2020-10-04 09:39:09 -0700

    added client-facing handlers for mongoDB-related errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,6 +1,26 @@
 //implementing one middleware for all error handling
 //express has built-in middleware for error handling, just specify err in the callback
 
+const AppError = require('../utils/appError');
+
+const handleIDErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateErrorDB = err => {
+  const value = err.keyValue.name;
+  const message = `Duplicate field value: ${value}. Please use another value`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+  // loop over the errors and map only their message properties
+  const errors = Object.values(err.errors).map(e => e.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   //we may have a status code and a message from our other error handling set up next(err)
   res.status(err.statusCode || 500).json({
@@ -19,11 +39,11 @@ const sendErrorProd = (err, res) => {
       status: err.status || 'failed',
       message: err.message || 'error',
     });
+
     // Programming or other unknown error: don't leak details to client
   } else {
     // 1) Log error
-    // !! for this mongoose error, why is message called "CastError" when logging the whole object, but not when logging it directly?
-    console.error('error 💥:', err, 'error message:', err.message);
+    console.error('error 💥:', err);
 
     // 2) Send generic message
     res.status(500).json({
@@ -32,11 +52,23 @@ const sendErrorProd = (err, res) => {
     });
   }
 };
+
 module.exports = (err, req, res, next) => {
 
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    console.log('err.name:', err.name);
+    // MongoDB errors will be Operational, but will not be marked as such in our code, so we have to account for them here. There are three types we are building handler blocks for
+    // 1) Invalid ID
+    if (err.path === '_id') {
+      error = handleIDErrorDB(error);
+    }
+    // 2) Duplicate field names in Create
+    if (err.code === 11000) error = handleDuplicateErrorDB(error);
+    // 3) Validation Errors in Update
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(error);
+    sendErrorProd(error, res);
   }
 };
