5e84493261aa7f2b80eb3ea42a46abe81a9fd850
Nicole Ticknor <36491978+nicoleticknor@users.noreply.github.com>
2020-10-04 11:42:17 -0700

    password encryption with bcrypt

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -24,14 +25,28 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
-    // he hasn't done any validation here yet
-    // validate: {
-    //   validator: function (val) {
-    //     return val === this.password;
-    //   },
-    //   message: 'Password confirmation does not match password',
-    // },
+    // !! This only works on the create and save methods, not the update method
+    validate: {
+      validator: function (val) {
+        return val === this.password;
+      },
+      message: 'Password confirmation does not match password',
+    },
   },
+});
+
+//doing encryption on the model (rather than the controller), because it's to do with the data, rather than the business logic
+userSchema.pre('save', async function (next) {
+  //gate clause to exit out of this function if the password hasn't changed
+  if (!this.isModified('password')) return next();
+
+  //The default salting value is 10, but since computer CPU power has increased over the years, it's better to use 12 now
+  //need to make this async  so we don't block the event loop from other users saving their information
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //we only needed this field for the signup validation. Once we click save, we want to delete that / not persist it in the database, so we are setting it to undefined
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
