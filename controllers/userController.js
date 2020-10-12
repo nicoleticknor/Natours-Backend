289629cac477802fc6ef89181217b13f6250c662
Nicole Ticknor <36491978+nicoleticknor@users.noreply.github.com>
2020-10-12 08:48:36 -0700

    route for users to update their name and email

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,15 @@
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  // check if each object key is one of the allowed fields
+  Object.keys(obj).forEach((e) => {
+    if (allowedFields.includes(e)) newObj[e] = obj[e];
+  });
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
@@ -15,30 +25,61 @@ exports.getAllUsers = catchAsync(async (req, res) => {
   });
 });
 
+exports.updateMe = catchAsync(async (req, res, next) => {
+  //password update is in another handler in the authController
+
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword',
+        400
+      )
+    );
+  }
+  // 2) Update user document
+  /* we can't just use the update to be req.body, because then someone could force update their role to admin for example.
+  so we specify which fields are allowed to be updated in the function arguments */
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  // we can use findByIdAndUpdate now because we aren't dealing with passwords/sensitive data that needs the save middleware to run
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    // new set to true so that it returns the updated object, not the old one
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    }
+  });
+});
+
 exports.getUser = (req, res) => {
   res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined",
+    status: 'error',
+    message: 'This route is not yet defined',
   });
 };
 
 exports.createUser = (req, res) => {
   res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined",
+    status: 'error',
+    message: 'This route is not yet defined',
   });
 };
 
 exports.updateUser = (req, res) => {
   res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined",
+    status: 'error',
+    message: 'This route is not yet defined',
   });
 };
 
 exports.deleteUser = (req, res) => {
   res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined",
+    status: 'error',
+    message: 'This route is not yet defined',
   });
 };
