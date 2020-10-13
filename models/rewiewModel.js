75f8af0b122f0ec7f9418d83dca7c9b664c443e0
Nicole Ticknor <36491978+nicoleticknor@users.noreply.github.com>
2020-10-13 09:57:49 -0700

    created review model with parent referencing to user and tour

---
+++ models/rewiewModel.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+// review / rating / createdAt / ref to tour / ref to user
+
+const reviewSchema = new mongoose.Schema(
+  {
+    rewiew: {
+      type: String,
+      required: [true, 'A review cannot be empty'],
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, 'A review must have a rating'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+
+    // ?? this is how to implement a parent reference
+
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user'],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
