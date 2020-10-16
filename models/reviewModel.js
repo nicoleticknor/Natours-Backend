f81aad1722c5d6d8d629e73d297e797ab3e8ffff
Nicole Ticknor <36491978+nicoleticknor@users.noreply.github.com>
2020-10-16 11:15:02 -0700

    calculating tour ratingsAverage based on reviews of that tour

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-// review / rating / createdAt / ref to tour / ref to user
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -57,6 +57,67 @@ reviewSchema.pre(/^find/, function (next) {
     select: 'name photo',
   });
   next();
+});
+
+// * calculating ratingsAverage based on review ratings for that tourId
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  // in static methods, this keyword points to the model directly. So we can call aggregate
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  // * persist the calculation to the database
+  // if the stats array is empty (as the case would be upon deleting the one and only review on a tour), then stats[0] will be undefined
+  // therefore undefined.nRating would cause an error, so guard clause against this
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      // this is the default rating upon creating a new tour
+      ratingsAverage: 4.5,
+    });
+  }
+};
+
+// * middleware to calculate the average rating
+// needs to be a post method because we need to have access to all the reviews (after saving the new one)
+// remember, post doesn't get access to next()
+reviewSchema.post('save', function () {
+  // this keyword points to current review, but constructor points to the model, which we haven't defined yet in a variable
+  this.constructor.calcAverageRatings(this.tour);
+});
+
+// * persisting the average rating in the database
+// findByIdAndUpdate and findByIdAndDelete only have query middleware available (we can't post with it), which are pre methods
+// we need to grab the review document so that we can access the tour ID and the constructor method calcAverageRatings after the post method above happens, and then pass that to the next middleware which is a post method
+// we are using a pre on the findOneAnd(update, delete) methods because that's how the findByIdAnd... methods work behind the scenes
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  // this keyword is the current *query* (the regex argument), not the document (the review). so we want to execute the query to get access to the document
+  // we are storing the review document to this.r so that it is accessible in the next middleware (on the this keyword)
+  this.r = await this.findOne();
+  next();
+});
+// * persisting the average rating in the database, continued
+// this post method is where we actually do the updating
+reviewSchema.post(/^findOneAnd/, async function () {
+  // await this.findOne() does not work here; query has already been executed
+  // from this.r we have access to the constructor methods
+  if (this.r) {
+    await this.r.constructor.calcAverageRatings(this.r.tour);
+  }
 });
 
 const Review = mongoose.model('Review', reviewSchema);
